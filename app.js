06c71721919411d801d6719f0724c253e8afb90c
code-with-smart <smartfaiza53@gmail.com>
2023-09-25 16:36:22 +0500

    js practice

---
+++ app.js
@@ -0,0 +1,252 @@
+// ------------------------------------ Chapter 21 (Changing Case) -----------------------------------//
+
+// ------ Ques 1
+
+// var userInput = "HELLO WORLD";
+// allLower = userInput.toLowerCase();
+
+
+// ------ Ques 2
+
+// var x = "XYZ";
+// x = x.toLowerCase();
+
+
+// ------ Ques 3
+
+// var y = "string";
+// y = y.toUpperCase();
+// var z = y.toLowerCase();
+
+
+// ------ Ques 4
+
+// var first = "FOTTBALL";
+// first = first.toLowerCase();
+// var result = console.log(first);
+
+
+// ------ Ques 5
+
+// var arr = ["CRICKET"];
+// for(var i =0 ;i < arr.length ; i++){
+// var dynamic= arr[i].toLowerCase();
+
+// ------ Ques 6
+
+// var theString = 'aString';
+// alert(theString.toUpperCase());
+
+// ------ Ques 7
+
+// var cityName = "kaRacHi"
+// var cityName = cityName.slice(0, 1).toUpperCase() + cityName.slice(1).toLowerCase()
+
+
+// ------------------------------------ Chapter 22 - 25 (Strings) -----------------------------------//
+
+
+// ------ Ques 1
+
+// var sameWords = "captain";
+// console.log(sameWords.slice(1, 3));
+// sameWords = sameWords.length;
+
+
+// ------ Ques 2
+
+// var animal = "elephant";
+// var seg = animal.slice(2, 6);
+
+
+// ------ Ques 3
+
+// var animal = "elephant"
+// var seg = animal.slice(2, 6)
+
+
+// ------ Ques 4
+
+// var first = "faheem";
+// first = first.length;
+// var result = console.log(first)
+
+
+// ------ Ques 5
+
+// var first = "examplestatement";
+// charcount = first.length;
+// console.log(charcount);
+
+// var slicestring  = first.charAt(0) + first.slice(1,2+11)
+// console.log(slicestring)
+
+
+// ------ Ques 6
+
+// var text = "To be or not to be."
+// var indx = text.indexOf("be")
+
+// ------ Ques 7
+
+// var text = "To be or not to be."; 
+// var indx = text.lastIndexOf("be");
+// console.log(indx) 
+
+
+// ------ Ques 8
+
+
+// var text = " Its match time lets go babby ";
+// var indx = text.indexOf("go");
+// console.log(indx)
+
+
+// ------ Ques 9
+
+
+
+// ------ Ques 10
+
+// var string = "abcde";
+// var index = string.charAt(2);
+//  //  answer is c
+
+// ------ Ques 11
+
+// var string = "abcdefghijkl";
+// var cha = string.charAt(10);
+//  console.log(cha);
+
+// ------ Ques 12
+
+// var str = "abcdefghijh";
+// var x = str.charAt(7);
+// console.log(x)
+
+
+// ------ Ques 13
+
+// var str = "moon";
+// var cha = str.charAt(4);
+// console.log(cha);
+
+
+// ------ Ques 14
+
+// NO
+
+// ------ Ques 15
+
+// NO
+
+// ------------------------------------ Chapter 26 (Rounding Numbers) -----------------------------------//
+
+
+// ------ Ques 1
+
+// var number = 2.46234
+// Math.round(number)
+
+// ------ Ques 2
+
+// var origNum = 3.14159
+
+// ------ Ques 3
+
+// var roundNum = Math.ceil(number)
+// var roundNum = Math.floor(number)
+
+// ------ Ques 4
+
+// var newResult = Math.round(number)
+
+// ------ Ques 5
+
+// var round = Math.floor(0.5)
+
+
+// ------------------------------------ Chapter 27 (Random Numbers) -----------------------------------//
+
+
+// ------ Ques 1
+
+// var random = Math.ceil(Math.random() * 50)
+
+
+// ------ Ques 2
+
+// var newRandomNum = Math.random();
+
+
+// ------ Ques 3
+
+// var dice = Math.floor(Math.random() * 6)
+
+
+// ------ Ques 4
+
+// var toss = Math.floor(Math.random() * 2)
+
+
+// ------------------------------------ Chapter 28, 29 (Converting Strings) -----------------------------------//
+
+
+// ------ Ques 1
+
+// parseInt("22")
+
+// ------ Ques 2
+
+// parseInt("123")
+
+// ------ Ques 3
+
+// parseFloat("123.465")
+
+// ------ Ques 4
+
+// var stringNum ="13"
+// alert(isNaN(stringNum - 0))
+
+// ------ Ques 5
+
+// var int = 22
+// int = int.toString()
+
+
+// ------ Ques 6
+
+// var int = 42
+// int = int.toString()
+
+// ------ Ques 7
+
+// parseInt("3.142")
+
+
+
+// ------------------------------------ Chapter 30 (Controlling the length of decimals) -----------------------------------//
+
+
+// ------ Ques 1
+
+// var aNumber = 3.14159
+// var newNum = aNumber.toFixed(4)
+
+// ------ Ques 2
+
+// var aNumber = parseFloat(aNumber.toFixed(2))
+
+
+// ------ Ques 3
+
+// var num = 123.354
+// if (num.toFixed(2).length > 4) { }
+
+// ------ Ques 4
+
+// var aNumber = 3.14159
+// alert(aNumber.toFixed(2))
+
+// ---------------------------------------- End --------------------------------------- //
